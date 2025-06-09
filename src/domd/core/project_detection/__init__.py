a8da5d49c4b67a2dc919fe87f058424d02f96603
tom-sapletta-com <tom@sapletta.com>
2025-06-09 10:37:25 +0200

    refactoring

---
+++ src/domd/core/project_detection/__init__.py
@@ -0,0 +1,5 @@
+"""Project detection module for finding and managing project commands."""
+
+from domd.core.project_detection.detector import ProjectCommandDetector
+
+__all__ = ["ProjectCommandDetector"]
